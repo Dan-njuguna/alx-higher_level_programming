d2d89231aaeeed5722aef31cc590899d25533a38
Dan-njuguna <njugush020@gmail.com>
2024-01-18 00:12:34 +0300

    Updated PAC

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''
+ islower: checks is a given character if lowercase or uppercase
+ @c: character to check if lowercase
+ Return: True if c is lowercase and False if otherwise
+'''
+
+def islower(c):
+    

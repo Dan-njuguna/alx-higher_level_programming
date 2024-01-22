a918a2ee9478d7b9e9b40663f3181d021a390633
Dan-njuguna <njugush020@gmail.com>
2024-01-22 20:52:48 +0300

    0-print_list_integer.py

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''
+ print_list_integer - prints each integer in a list on a new line
+ @my_list: a list of integers
+ Return: Nothing
+'''
+
+
+def print_list_integer(my_list=[]):
+    for i in my_list:
+        print("{}".format(i))

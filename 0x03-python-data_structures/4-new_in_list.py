499809525a1b9b80aecb5d832ea519eba1991994
Dan-njuguna <njugush020@gmail.com>
2024-01-23 10:21:33 +0300

    4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 
 '''
+ new_in_list - returns new list
+ @my_list: older list
+ @idx: index of element to replace
+ @element: new element
+ Return: new list
 '''
 
 
@@ -8,7 +13,7 @@ def new_in_list(my_list, idx, element):
     new_list = []
     for item in my_list:
         new_list.append(item)
-    
+
     if idx >= 0 and idx < len(new_list):
         new_list[idx] = element
 

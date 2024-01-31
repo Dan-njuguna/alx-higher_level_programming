55fbb52aa41d16852a9988d3c613d6092c671000
Dan-njuguna <njugush020@gmail.com>
2024-01-31 09:26:37 +0300

    3-common_elements.py

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 
 def common_elements(set_1, set_2):
-    commons = set(set_1, set_2)
-    return commons
+    item = []
+    for item_1 in set_1:
+        if item_1 in set_2:
+            item.append(item_1)
+
+    return item

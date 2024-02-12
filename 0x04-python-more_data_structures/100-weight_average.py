6d8e8743b4b25b2e766b347db013c1b9a5960b56
Dan-njuguna <njugush020@gmail.com>
2024-02-12 11:59:43 +0300

    100-weight_average.py

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''
+ weight_average - returns the weighted average of all integers tuple (<score>, <weight>)
+ @my_list: a list
+ Return: 0 if list empty, otherwise average
+'''
+
+
+def weight_average(my_list=[]):
+    llist = []
+    deno = []
+    for tup in my_list:
+        a, b = tup
+        ans = a * b
+        llist.append(ans)
+        deno.append(b)
+    
+    avg = sum(llist)/sum(deno)
+         
+    return avg

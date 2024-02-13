fb16ab11625894905b826d618243b15d20aac848
Dan-njuguna <njugush020@gmail.com>
2024-02-13 22:21:30 +0300

    1-safe_print_integer.py

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(int(value)))
+        return True
+
+    except ValueError:
+        return False
+
+    except Exception:
+        return False

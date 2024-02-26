0ee71f80719a38b7365bd8c97bc6bdad94c6b991
Dan-njuguna <njugush020@gmail.com>
2024-02-26 09:23:25 +0300

    1-sqaure.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 class Square:
+    """Defines a square."""
     def __init__(self, size):
+        """Constructor.
+        Args:
+            size: length of side of the square.
+        """
         self.__size = size
